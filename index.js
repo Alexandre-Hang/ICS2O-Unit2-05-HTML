37ec86a8903340c002d532ce39884bbb1bd0f9b8
Ubuntu <ubuntu@ip-172-31-93-59.ec2.internal>
2020-03-16 21:44:19 +0000

    Added all

---
+++ index.js
@@ -0,0 +1,8 @@
+// JavaScript File
+function calcArea() {
+let squareLength
+    ,squareArea;
+squareLength = document.getElementById("squareLength").value;
+squareArea = squareLength * squareLength;
+alert("The area of the square is "+ squareArea);
+}
